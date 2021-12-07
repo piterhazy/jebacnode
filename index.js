dccffd01a4383ec7242d3be5038492b2abe1c2c2
piterhazy <piterhazy@gmail.com>
2021-12-07 15:01:55 +0100

    express czesc 3

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-const express = require('express');
-const app = express();
-const square = require('./square'); // Here we require() the name of the file without the (optional) .js file extension
-console.log('The area of a square with a width of 4 is ' + square.area(4));
-module.exports = {
-    area: function(width) {
-        return width * width;
-    },
-
-    perimeter: function(width) {
-        return 4 * width;
-    }
-};
+console.log('First');
+console.log('Second');
+setTimeout(function() {
+    console.log('First');
+}, 3000);
+console.log('Second');
