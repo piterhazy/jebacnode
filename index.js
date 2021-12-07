e909bd694423f8661b19d8900c36df54c257000c
piterhazy <piterhazy@gmail.com>
2021-12-07 15:13:19 +0100

    express czesc 4

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-console.log('First');
-console.log('Second');
-setTimeout(function() {
-    console.log('First');
-}, 3000);
-console.log('Second');
+const express = require('express');
+const app = express();
+app.get('/', function(req, res) {
+    res.send('Hello World!')
+});
+app.all('/secret', function(req, res, next) {
+    console.log('Accessing the secret section ...');
+    next(); // pass control to the next handler
+});
+const wiki = require('./wiki.js');
+// ...
+app.use('/wiki', wiki);
