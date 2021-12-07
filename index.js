b4121a05a1cbd9e40ba97c8c2153ea78797eaf83
piterhazy <piterhazy@gmail.com>
2021-12-07 14:59:03 +0100

    express czesc 2

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const square = require('./square'); // Here we require() the name of the file without the (optional) .js file extension
+console.log('The area of a square with a width of 4 is ' + square.area(4));
+module.exports = {
+    area: function(width) {
+        return width * width;
+    },
 
-app.get('/', function(req, res) {
-    res.send('Hello World!')
-});
-
-app.listen(port, function() {
-    console.log(`Example app listening on port ${port}!`)
-});
+    perimeter: function(width) {
+        return 4 * width;
+    }
+};
