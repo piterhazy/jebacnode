9f9b2007916e33387b626cac9311f5111cc83c6d
piterhazy <piterhazy@gmail.com>
2021-12-07 14:53:46 +0100

    express czesc 1

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const http = require("http");
-
-const hostname = "127.0.0.1";
-const port = 8000;
-
-
-const server = http.createServer(function(req, res) {
-
-
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-
-
-    res.end('pierdolic orka\n');
+app.get('/', function(req, res) {
+    res.send('Hello World!')
 });
 
-
-server.listen(port, hostname, function() {
-    console.log(`jebac disa na http://${hostname}:${port}/`);
-})
+app.listen(port, function() {
+    console.log(`Example app listening on port ${port}!`)
+});
